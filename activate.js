ae6ad24e29b23927b204a5286a4582fae0eecc6e
Andrei Otea <otea@optymyze.com>
2019-05-07 17:35:18 +0300

    Hoisting

--- activate.js
+++ activate.js
@@ -1,15 +1,13 @@
-var handleMouseOver = handleMouseOut = removeDomElement = undefined;
-
 var style = document.createElement('style');
 	style.type = 'text/css';
 	style.innerHTML = '.hovered-item { border: 2px dashed #000; }';
 
-handleMouseOver = event => event.target.classList.add('hovered-item');
-handleMouseOut = event => event.target.classList.remove('hovered-item');
-removeDomElement = event => {
-	event.preventDefault();
-	event.target.remove();
-};
+var handleMouseOver = event => event.target.classList.add('hovered-item'),
+	handleMouseOut = event => event.target.classList.remove('hovered-item'),
+	removeDomElement = event => {
+		event.preventDefault();
+		event.target.remove();
+	};
 
 document.getElementsByTagName('head')[0].appendChild(style);
 window.addEventListener('mouseover', handleMouseOver);
